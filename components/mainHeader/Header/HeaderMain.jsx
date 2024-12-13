72ee546c3fb91bf40eddc6610c1f845ee47f553f
davidhome <ducanhnguyen145@gmail.com>
2024-12-13 20:27:28 +0300

    styling home component

---
+++ components/mainHeader/Header/HeaderMain.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import styles from "./HeaderMain.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "../MainHeaderBackground/MainHeaderBackground";
+
+const HeaderMain = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image
+            src={logoImg.src}
+            alt="a plate with foot on it"
+            width={500}
+            height={100}
+            priority
+          />
+          NextLevel Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Browse Meals</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default HeaderMain;
